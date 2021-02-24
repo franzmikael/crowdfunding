291a70dc169e29eb3652495573f3f5c9c9f0106a
Franz Mikael <franzmikael11@gmail.com>
2021-02-24 22:26:04 +0700

    Get campaigns endpoint slug

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -7,6 +7,7 @@ type CampaignFormatter struct {
 	ImageUrl         string `json:"image_url"`
 	GoalAmount       int    `json:"goal_amount"`
 	CurrentAmount    int    `json:"current_amount"`
+	Slug             string `json:"slug"`
 	UserID           int    `json:"user_id"`
 }
 
@@ -18,6 +19,7 @@ func FormatCampaign(campaign Campaign) CampaignFormatter {
 		ImageUrl:         "",
 		GoalAmount:       campaign.GoalAmount,
 		CurrentAmount:    campaign.CurrentAmount,
+		Slug:             campaign.Slug,
 		UserID:           campaign.UserID,
 	}
 
