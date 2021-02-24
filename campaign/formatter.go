142524830d6231635acc33d30d84864d05f7f22c
Franz Mikael <franzmikael11@gmail.com>
2021-02-24 22:16:39 +0700

    Get campaigns endpoint formatter

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,40 @@
 package campaign
+
+type CampaignFormatter struct {
+	ID               int    `json:"id"`
+	Title            string `json:"title"`
+	ShortDescription string `json:"short_description"`
+	ImageUrl         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+	UserID           int    `json:"user_id"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	formatter := CampaignFormatter{
+		ID:               campaign.ID,
+		Title:            campaign.Name,
+		ShortDescription: campaign.ShortDescription,
+		ImageUrl:         "",
+		GoalAmount:       campaign.GoalAmount,
+		CurrentAmount:    campaign.CurrentAmount,
+		UserID:           campaign.UserID,
+	}
+
+	if len(campaign.CampaignImages) > 0 {
+		formatter.ImageUrl = campaign.CampaignImages[0].FileName
+	}
+
+	return formatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	campaignsFormatter := []CampaignFormatter{}
+
+	for _, campaign := range campaigns {
+		campaignFormatter := FormatCampaign(campaign)
+		campaignsFormatter = append(campaignsFormatter, campaignFormatter)
+	}
+
+	return campaignsFormatter
+}
